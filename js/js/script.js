a3034f0e045de8100ea1c75f4285dfb0868975d2
ermohitjoshi <87078547+ermohitjoshi@users.noreply.github.com>
2022-09-07 21:27:18 +0530

    Add files via upload

---
+++ js/js/script.js
@@ -0,0 +1,30 @@
+// --------------------------------------ABOUT TABS-------------------------------------------\\
+
+const   tabsContainer = document.querySelector(".about-tabs")
+aboutSection = document.querySelector(".about-section");
+
+tabsContainer.addEventListener("click", (e) =>{
+    if(e.target.classList.contains("tab-item")  && !e.target.classList.contains("active")){
+        tabsContainer.querySelector(".active").classList.remove("active");
+        e.target.classList.add("active");
+        const target = e.target.getAttribute("data-target");
+        aboutSection.querySelector(".tab-content.active").classList.remove("active");
+        aboutSection.querySelector(target).classList.add("active");
+    };
+});
+
+// --------------------------------------TOGGLE NAVBAR-------------------------------------------\\
+
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", () =>{
+    hideSection();
+    toggleNavbar();
+
+});
+
+function hideSection(){
+    document.querySelector("section.active").classList.toggle("fade-out")
+};
+function toggleNavbar(){
+    document.querySelector(".header").classList.toggle("active")
+};
